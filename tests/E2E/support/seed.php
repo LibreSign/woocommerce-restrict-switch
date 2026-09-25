2518540ed8e6321658f73e9776a4cbf6ad558b0e
YvesCesar <yvesamorim73@gmail.com>
2026-09-25 11:41:42 -0400

    test: name the e2e seed variables after what they hold
    Signed-off-by: YvesCesar <yvesamorim73@gmail.com>

--- tests/E2E/support/seed.php
+++ tests/E2E/support/seed.php
@@ -2,34 +2,34 @@
 
 wc_transaction_query( 'start' );
 
-$wrd_plans = array();
+$plans = array();
 
-foreach ( array( 'Basic', 'Professional', 'Enterprise' ) as $wrd_name ) {
-	$wrd_plan = new WC_Product_Subscription();
-	$wrd_plan->set_name( $wrd_name );
-	$wrd_plan->set_regular_price( '10' );
-	$wrd_plan->set_virtual( true );
-	$wrd_plan->update_meta_data( '_subscription_price', '10' );
-	$wrd_plan->update_meta_data( '_subscription_period', 'month' );
-	$wrd_plan->update_meta_data( '_subscription_period_interval', '1' );
-	$wrd_plan->update_meta_data( 'restrict_herself_upsells_switch', 'yes' );
-	$wrd_plan->save();
+foreach ( array( 'Basic', 'Professional', 'Enterprise' ) as $plan_name ) {
+	$plan = new WC_Product_Subscription();
+	$plan->set_name( $plan_name );
+	$plan->set_regular_price( '10' );
+	$plan->set_virtual( true );
+	$plan->update_meta_data( '_subscription_price', '10' );
+	$plan->update_meta_data( '_subscription_period', 'month' );
+	$plan->update_meta_data( '_subscription_period_interval', '1' );
+	$plan->update_meta_data( 'restrict_herself_upsells_switch', 'yes' );
+	$plan->save();
 
-	$wrd_plans[ $wrd_name ] = $wrd_plan;
+	$plans[ $plan_name ] = $plan;
 }
 
-foreach ( array( 'Basic', 'Professional' ) as $wrd_name ) {
-	$wrd_plans[ $wrd_name ]->set_upsell_ids( array( $wrd_plans['Enterprise']->get_id() ) );
-	$wrd_plans[ $wrd_name ]->save();
+foreach ( array( 'Basic', 'Professional' ) as $plan_name ) {
+	$plans[ $plan_name ]->set_upsell_ids( array( $plans['Enterprise']->get_id() ) );
+	$plans[ $plan_name ]->save();
 }
 
-$wrd_group = new WC_Product_Grouped();
-$wrd_group->set_name( 'Upgrade subscription' );
-$wrd_group->set_slug( 'upgrade-subscription' );
-$wrd_group->set_children( array_map( static fn ( $plan ) => $plan->get_id(), array_values( $wrd_plans ) ) );
-$wrd_group->save();
+$upgrade_group = new WC_Product_Grouped();
+$upgrade_group->set_name( 'Upgrade subscription' );
+$upgrade_group->set_slug( 'upgrade-subscription' );
+$upgrade_group->set_children( array_map( static fn ( $plan ) => $plan->get_id(), array_values( $plans ) ) );
+$upgrade_group->save();
 
-$wrd_customer = wp_insert_user(
+$customer_id = wp_insert_user(
 	array(
 		'user_login' => 'customer',
 		'user_pass'  => 'password',
@@ -38,21 +38,21 @@ $wrd_customer = wp_insert_user(
 	)
 );
 
-$wrd_order = wc_create_order( array( 'customer_id' => $wrd_customer ) );
-$wrd_order->add_product( $wrd_plans['Basic'] );
-$wrd_order->calculate_totals();
-$wrd_order->update_status( 'completed' );
+$first_order = wc_create_order( array( 'customer_id' => $customer_id ) );
+$first_order->add_product( $plans['Basic'] );
+$first_order->calculate_totals();
+$first_order->update_status( 'completed' );
 
-$wrd_subscription = wcs_create_subscription(
+$subscription = wcs_create_subscription(
 	array(
-		'order_id'         => $wrd_order->get_id(),
-		'customer_id'      => $wrd_customer,
+		'order_id'         => $first_order->get_id(),
+		'customer_id'      => $customer_id,
 		'billing_period'   => 'month',
 		'billing_interval' => 1,
 	)
 );
-$wrd_subscription->add_product( $wrd_plans['Basic'] );
-$wrd_subscription->calculate_totals();
-$wrd_subscription->update_status( 'active' );
+$subscription->add_product( $plans['Basic'] );
+$subscription->calculate_totals();
+$subscription->update_status( 'active' );
 
 wc_transaction_query( 'commit' );
